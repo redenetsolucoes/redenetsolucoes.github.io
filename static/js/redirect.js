3c21918f2701d6da2670bec03d4765c77c4e757b
Dijalma Silva <dijalmacz@gmail.com>
2017-01-30 14:45:20 -0300

    Push the files

---
+++ static/js/redirect.js
@@ -0,0 +1,11 @@
+function redirectOnlineCajazeiras(){
+  window.location.replace("http://www.iescolaronline.com.br/");
+};
+
+function redirectOnlineBayeux(){
+  window.location.replace("http://www.educabayeux.com.br/");
+};
+
+function redirectOnlineSantaHelena(){
+  window.location.replace("http://www.educasantahelena.com.br/");
+};
