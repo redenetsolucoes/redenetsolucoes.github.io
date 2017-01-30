ac8f6663a92a9d0e499e8cd174e86df4749a3b06
Dijalma Silva <dijalmacz@gmail.com>
2017-01-30 15:40:53 -0300

    Adicionando loader na requisicao

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -1,11 +1,24 @@
+$(document).ready(function() {
+
+  $('#loader').fadeOut(200);
+});
+
 function redirectOnlineCajazeiras(){
   window.location.replace("http://www.iescolaronline.com.br");
+  showLoading();
 };
 
 function redirectOnlineBayeux(){
   window.location.replace("http://www.educabayeux.com.br/");
+  showLoading();
 };
 
 function redirectOnlineSantaHelena(){
   window.location.replace("http://www.educasantahelena.com.br/");
+  showLoading();
 };
+
+
+function showLoading(){
+  $('#loader').show();
+}
